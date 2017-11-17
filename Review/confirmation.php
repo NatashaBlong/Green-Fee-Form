360228398b98afe0effb02673cb9a3689f92d6fc
Natasha Blong <natashablong@yahoo.com>
2017-11-16 19:39:32 -0600

    stuff

--- Review/confirmation.php
+++ Review/confirmation.php
@@ -1,39 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Confirmation</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.
-        2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2r
-        RUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<link rel="stylesheet" href="fishcreek.css">
+  <title>test result page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <script src="jScript.js"></script>
 </head>
 <body>
-<h1>Complete</h1>
+  <!--  Top header   -->
+  <div class="jumbotron">
+    <div class="container text-center">
+      <h1>Student Green Fee Review</h1>
+      <p>put something here</p>
+    </div>
+  </div>
 
+  <!--  side bar   -->
+  <div class="container-fluid">
+    <div class="row content">
+      <div class="col-sm-2 sidenav" class="sidebars">
+      </div>
 
+      <!--  questions to enter quiz   -->
+      <div class="col-sm-8">
 
-<?php 
-	$c=0;
-	$d=100;
-	$db = new PDO("mysql:dbname=344Database", "root", "");
-	$ques = $db->query("SELECT question FROM questions");
-    foreach($ques as $que) {
-		$x = $_POST[$c];
-		$y = $_POST[$d];
-    $lines = $db->query("INSERT INTO `Scores` (`Question`,`Comment`,`Score`) VALUES ('".$que["question"]."', '".$y."','".$x."');");
-	$c=$c+1;
-	$d=$d+1;
-	}
-?>
+				<h1 class="container text-center">Complete</h1>
+				<?php
+					$c=0;
+					$d=100;
+					$db = new PDO("mysql:dbname=344Database", "root", "");
+					$ques = $db->query("SELECT question FROM questions");
+				    foreach($ques as $que) {
+						$x = $_POST[$c];
+						$y = $_POST[$d];
+				    $lines = $db->query("INSERT INTO `Scores` (`Question`,`Comment`,`Score`) VALUES ('".$que["question"]."', '".$y."','".$x."');");
+					$c=$c+1;
+					$d=$d+1;
+					}
+				?>
 
+				<p>Your response for the proposals have been saved,
+				thank you for taking the time to review for Green Fee.</p>
 
+				<img src="checkmark.gif" alt="checkmark" width="700px" id="check" class="container text-center">
 
-<p>Your response for the proposals have been saved, 
-thank you for taking the time to review for Green Fee.</p>
-
-<img src="checkmark.gif" alt="checkmark" width="700px" id="check">
-
-	
-</body>
-</html>
+      </div>
+        <div class="col-sm-2 sidenav" class="fix">
+        </div>
+      </div>
+    </div>
+    <footer class="container-fluid">
+      <p>Copyright Green Fee</p>
+    </footer>
+  </body>
+  </html>
