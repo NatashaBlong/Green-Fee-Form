54b00b7fb349de095b341ed16ad9c71494b7a6cd
Natasha Blong <natashablong@yahoo.com>
2017-11-28 17:40:37 -0600

    fsdf

--- Final Project/Trial/review.php
+++ Final Project/Trial/review.php
@@ -5,25 +5,27 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel="stylesheet" href="reviewStyle.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <script src="jScript.js"></script>
 </head>
 <body>
   <!--  Top header   -->
   <div class="jumbotron">
     <div class="container text-center">
-      <h1>Student Green Fee Review</h1>
+      <h1>Student Green Fee Review - OLD DATA</h1>
       <p>put something here</p>
     </div>
   </div>
+
   <!--  side bar   -->
   <div class="container-fluid">
     <div class="row content">
       <div class="col-sm-2 sidenav" class="sidebars">
       </div>
+
       <!--  questions to enter quiz   -->
       <div class="col-sm-8">
         <?php
@@ -31,13 +33,18 @@
         <body onload="document.getElementById('id01').style.display='block'">
         <?php } ?>
           <div id="id01" class="modal">
+
             <form class="modal-content animate" action="review.php" method="POST">
+
               <div class="container">
                 <label><b><h3>Please Log in and select a proposal to review:</h3></b></label>
+
                 <label><b>Name</b></label>
                 <input type="text" placeholder="Enter Name" name="name" required ><br>
+
                 <label><b>Password</b></label>
                 <input type="password" placeholder="Enter Password" name="psw" required><br><br>
+
                 <label><b>Select a Proposal</b></label>
                 <select name="output" class="form-control" id="sell">
                   <?php
@@ -59,14 +66,17 @@
             </form>
           </div>
           <!--  end create arrays   -->
+
           <!--  start accordian box   -->
           <form action="confirmation.php" method="POST">
           <?php
           if (isset($_POST["output"]) && isset($_POST["name"]) && isset($_POST["psw"])) { ?>
             <div class="panel-group" id="accordion">
               <?php
+
               $pos = 1; //counter
               $posAvg = 0;
+
               $ques = $db->query("SELECT * FROM question");
               $i = 0;
               $j = 100;
@@ -80,7 +90,9 @@
                   </div>
                   <?php print '<div id="collapse'.$pos.'" class="panel-collapse collapse in">'?>
                     <div class="panel-body">
+
                       <div class="col-6" class="<?= $k ?>">
+
                         <div class="row">
                           <div class="col-xs-6">
                             <<?= $que["text"] ?> <br>
